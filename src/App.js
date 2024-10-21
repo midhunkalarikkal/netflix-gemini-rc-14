1babca5c7bc27c9e56847e20c2f7cf854bb473e8
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-10-22 02:21:55 +0530

    Body , Header Browse Login component created plain, Sample router configured

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="App">
-      Namaste everyOne lets built Netflix GPT
-    </div>
+    <Body />
   );
 }
 
