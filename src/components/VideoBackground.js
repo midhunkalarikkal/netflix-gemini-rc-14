8a212e653284215e688835405ea728b4e0fda380
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-10-26 02:38:46 +0530

    Main video autoplay fixed

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,15 +1,16 @@
 import { useSelector } from "react-redux";
 import useGetMovieTrailer from "../hooks/useGetMovieTrailer";
 
-const VideoBackground = ({movieId}) => {
+const VideoBackground = ({ movieId }) => {
   const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
-  
-  useGetMovieTrailer(movieId)
+
+  useGetMovieTrailer(movieId);
 
   return (
     <div className="w-full">
-      <iframe className="w-full aspect-video"
-        src={"https://www.youtube.com/embed/"+trailerVideo?.key+"?autoplay=1&controls=0&mute=1"}
+      <iframe
+        className="w-full aspect-video"
+        src={`https://www.youtube.com/embed/${trailerVideo?.key}?autoplay=1&controls=0&mute=1&loop=1&playlist=${trailerVideo?.key}`}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
