899bfa4213e25a5eaff4fae5126e57b9e45d2635
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-10-25 22:07:11 +0530

    movieCard design updated

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,7 +4,7 @@ import { IMG_CDN } from "../utils/constants"
 const MovieCard = ({ movieId}) => {
   const metaData = useSelector((store) => store.movies?.metaData.find((movie) => movie.id === movieId))
   return (
-    <div className="w-48 pr-1">
+    <div className="w-72">
         <img src={IMG_CDN+metaData?.poster} alt="movie_poster" />
     </div>
   )
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -5,7 +5,7 @@ const MovieList = ({ title, movies }) => {
     <div className="px-6 py-2">
       <h3 className="text-white font-bold  text-3xl py-4">{title}</h3>
       <div className="flex overflow-x-scroll no-scrollbar">
-        <div className="flex space-x-4">
+        <div className="flex space-x-2">
           {movies.map((movie) => (
             <MovieCard key={movie.id} movieId={movie.id} />
           ))}
