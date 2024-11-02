945ead7e2beda9305f9be458da2165a8aed9e00a
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-11-03 02:59:50 +0530

    Footer updated for the small screens

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className="bg-black text-white pt-16 pb-20 px-48 h-[30vh] custom-text-color">
+    <div className="bg-black text-white text-xs md:text-md lg:text-lg pt-16 pb-20 px-14 md:px-48 h-[30vh] custom-text-color">
       <div className="mb-6">
-        <p>Questions? Call 000-800-919-1694</p>
+        <p className="">Questions? Call 000-800-919-1694</p>
       </div>
 
-      <div className="grid grid-cols-4 gap-4">
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
         <div className="flex flex-col space-y-5">
           <Link to="/" className="underline custom-text-color">
             FAQ
@@ -15,6 +15,15 @@ const Footer = () => {
           <Link to="/" className="underline custom-text-color">
             Cookie Preferences
           </Link>
+          <div className="mt-6">
+            <button
+              className="py-1 px-6 border-white text-white"
+              style={{ border: "1px solid #b6b5b4" }}
+            >
+              {" "}
+              English{" "}
+            </button>
+          </div>
         </div>
         <div className="flex flex-col space-y-5">
           <Link to="/" className="underline custom-text-color">
@@ -34,9 +43,6 @@ const Footer = () => {
             Privacy
           </Link>
         </div>
-      </div>
-      <div className="mt-6">
-        <button className="py-1 px-6 border-white text-white" style={{border : "1px solid #b6b5b4"}}> English </button>
       </div>
     </div>
   );
