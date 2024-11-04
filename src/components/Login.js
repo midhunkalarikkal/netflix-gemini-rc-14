cd3fb85c8324daacbbcbdb0d06c1f620ddf9a8d1
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-11-04 17:11:06 +0530

    Forgot password feature implemented

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,6 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import toast from "react-hot-toast";
-import { Link } from "react-router-dom";
 import React, { useState } from "react";
 import { auth } from "../utils/firebase";
 import { useDispatch } from "react-redux";
@@ -9,16 +8,18 @@ import { useForm } from "react-hook-form";
 import { addUser } from "../utils/userSlice";
 import { LOGIN_BG_IMAGE } from "../utils/constants";
 import VisibilityIcon from "@mui/icons-material/Visibility";
-import {getFirebaseErrorMessage } from "../utils/validations";
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-import { createUserWithEmailAndPassword,
+import { getFirebaseErrorMessage } from "../utils/validations";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import {
+  createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  sendPasswordResetEmail,
   updateProfile,
 } from "firebase/auth";
 
-
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [isForgotPasswordForm, setIsForgotPasswordForm] = useState(false);
   const [errMessage, setErrMessage] = useState(null);
   const [showPassword, setShowPassword] = useState(false);
 
@@ -39,21 +40,35 @@ const Login = () => {
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
     setErrMessage(null);
+    setIsForgotPasswordForm(false);
   };
 
-  const onSubmit = (data,e) => {
+  const toggleToForgotPassword = () => {
+    setIsForgotPasswordForm((prev) => !prev);
+  };
+
+  const onSubmit = (data, e) => {
     e.preventDefault();
     if (!isValid) {
       toast.error("Please fix the errors before submitting the form.");
       return;
     }
 
-    if (!isSignInForm) {
-      createUserWithEmailAndPassword(
-        auth,
-        data.email,
-        data.password
-      )
+    if(isForgotPasswordForm){
+      sendPasswordResetEmail(auth, data.email)
+      .then(() => {
+        console.log("Email sended");
+        toast.success("Email sended successfully!")
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        console.log("errorCode : ",errorCode);
+        const errorMessage = error.message;
+        console.log("errorMessage : ",errorMessage);
+        setErrMessage(getFirebaseErrorMessage(error.code));
+      });
+    } else if (!isSignInForm) {
+      createUserWithEmailAndPassword(auth, data.email, data.password)
         .then((userCredential) => {
           const user = userCredential.user;
           updateProfile(user, {
@@ -77,11 +92,7 @@ const Login = () => {
           toast.error("Sign up failed. Please try again.");
         });
     } else {
-      signInWithEmailAndPassword(
-        auth,
-        data.email,
-        data.password
-      )
+      signInWithEmailAndPassword(auth, data.email, data.password)
         .then((userCredential) => {
           const user = userCredential.user;
           toast.success("Welcome " + user.displayName);
@@ -110,9 +121,14 @@ const Login = () => {
         className="absolute py-10 px-14 md:px-16 bg-black w-[85%] sm:w-[70%] md:w-[430px] flex flex-col top-20 left-1/2 transform -translate-x-1/2 text-white bg-opacity-75"
       >
         <h1 className="font-bold text-2xl lg:text-3xl py-1 lg:py-3 mb-3">
-          {isSignInForm ? "Sign In" : "Sign Up"}
+          {isForgotPasswordForm
+            ? "Forgot Password"
+            : isSignInForm
+            ? "Sign In"
+            : "Sign Up"}
         </h1>
-        {!isSignInForm && (
+
+        {!isSignInForm && !isForgotPasswordForm && (
           <>
             <input
               type="text"
@@ -127,88 +143,110 @@ const Login = () => {
               })}
             />
             {errors.name && (
-              <p className="text-red-500 text-sm md:text-md">{errors.name.message}</p>
+              <p className="text-red-500 text-sm md:text-md">
+                {errors.name.message}
+              </p>
             )}
           </>
         )}
+
         <>
-        <input
-          type="text"
-          placeholder="Email or mobile number"
-          className="p-2 my-1 md:p-4 md:my-2 w-full bg-gray-400 text-white bg-opacity-10 border-[1px] rounded-[4px] focus:border-white focus:outline-none custom-input"
-          {...register("email", {
-            required: "Email is required",
-            pattern: {
-              value: /^[a-zA-Z0-9._%+-]+@gmail\.com$/,
-              message: "Enter a valid email.",
-            },
-          })}
-          />
-        {errors.email && (
-          <p className="text-red-500 text-sm md:text-md">{errors.email.message}</p>
-        )}
-        </>
-        
-        <div className="relative w-full">
           <input
-            type={showPassword ? "text" : "password"}
-            placeholder="Password"
+            type="text"
+            placeholder="Email Id"
             className="p-2 my-1 md:p-4 md:my-2 w-full bg-gray-400 text-white bg-opacity-10 border-[1px] rounded-[4px] focus:border-white focus:outline-none custom-input"
-            {...register("password", {
-              required: "Password is required",
+            {...register("email", {
+              required: "Email is required",
               pattern: {
-                value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/,
-                message: "Enter a valid password.",
+                value: /^[a-zA-Z0-9._%+-]+@gmail\.com$/,
+                message: "Enter a valid email.",
               },
             })}
           />
-          <span
-            onClick={togglePasswordVisibility}
-            className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer text-gray-300 opacity-75"
-          >
-            {
-              showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />
-            }
-          </span>
-          {errors.password && (
-          <p className="text-red-500 text-sm md:text-md">{errors.password.message}</p>
+          {errors.email && (
+            <p className="text-red-500 text-sm md:text-md">
+              {errors.email.message}
+            </p>
+          )}
+        </>
+
+        {!isForgotPasswordForm && (
+          <div className="relative w-full">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+              className="p-2 my-1 md:p-4 md:my-2 w-full bg-gray-400 text-white bg-opacity-10 border-[1px] rounded-[4px] focus:border-white focus:outline-none custom-input"
+              {...register("password", {
+                required: "Password is required",
+                pattern: {
+                  value:
+                    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/,
+                  message: "Enter a valid password.",
+                },
+              })}
+            />
+            <span
+              onClick={togglePasswordVisibility}
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer text-gray-300 opacity-75"
+            >
+              {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+            </span>
+            {errors.password && (
+              <p className="text-red-500 text-sm md:text-md">
+                {errors.password.message}
+              </p>
+            )}
+          </div>
         )}
-        </div>
+
         {errMessage && (
           <p className="py-2 my-1 font-semibold text-xs md:text-md text-red-600">
             {errMessage}
           </p>
         )}
+
         <button
           type="submit"
           className="px-4 py-1 mt-2 md:py-2 w-full font-semibold rounded-[4px]"
           style={{ backgroundColor: "#E50914" }}
-          // onClick={handleSubmitButton}
         >
-          {isSignInForm ? "Sign In" : "Sign Up"}
+          {isForgotPasswordForm
+            ? "Submit email"
+            : isSignInForm
+            ? "Sign In"
+            : "Sign Up"}
         </button>
-        <span className="px-2 text-gray-300 opacity-80 text-center text-sm md:text-md py-1 md:py-2">
-          OR
-        </span>
-        <button className="px-4 py-1 md:py-2 my-1 w-full font-semibold rounded-[4px] bg-white bg-opacity-25 hover:bg-opacity-20 transition duration-200 ease-in-out transform">
-          Use a sign-in code
-        </button>
-        <Link
-          to="/"
-          className="my-2 text-sm md:text-md text-center text-white hover:text-[#b6b5b4] hover:underline"
-        >
-          Forgot password?
-        </Link>
-        <div className="flex items-center text-sm md:text-md mt-2">
-          <input
-            className="h-4 w-4 cursor-pointer custom-checkbox rounded-[4px]"
-            type="checkbox"
-            id="rememberMe"
-          />
-          <label htmlFor="rememberMe" className="ml-2">
-            Remember me
-          </label>
-        </div>
+
+        {!isForgotPasswordForm && (
+          <>
+            <span className="px-2 text-gray-300 opacity-80 text-center text-sm md:text-md py-1 md:py-2">
+              OR
+            </span>
+
+            <button className="px-4 py-1 md:py-2 my-1 w-full font-semibold rounded-[4px] bg-white bg-opacity-25 hover:bg-opacity-20 transition duration-200 ease-in-out transform">
+              Use a sign-in code
+            </button>
+
+            <p
+              className="my-2 text-sm md:text-md text-center text-white hover:text-[#b6b5b4] hover:underline"
+              onClick={toggleToForgotPassword}
+            >
+              Forgot password?
+            </p>
+
+            <div className="flex items-center text-sm md:text-md mt-2">
+              <input
+                className="h-4 w-4 cursor-pointer custom-checkbox rounded-[4px]"
+                type="checkbox"
+                id="rememberMe"
+              />
+              <label htmlFor="rememberMe" className="ml-2">
+                Remember me
+              </label>
+            </div>
+          </>
+        )}
+
         <p
           className="py-4 cursor-pointer text-sm md:text-md -mt-1 mb-2"
           onClick={toggleSignInForm}
@@ -225,6 +263,15 @@ const Login = () => {
             </span>
           )}
         </p>
+
+        {isForgotPasswordForm && (
+            <p
+            className="text-xs md:text-[13px] custom-text-color pb-6"
+              >
+          You will get an email for updating your password
+        </p>
+        )}
+
         <p
           className={`text-xs md:text-[13px] custom-text-color ${
             isSignInForm ? "pb-10 md:pb-28" : "pb-2"
