c39e1474abe47691d8e36b3a5b1c87e98247f813
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-01-30 02:58:35 +0530

    update checked

--- src/components/BrowserMainContainer/MainContainer.js
+++ src/components/BrowserMainContainer/MainContainer.js
@@ -2,6 +2,8 @@ import VideoTitle from "./VideoTitle";
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import VideoBackground from "./VideoBackground";
+import VideoBackgroundShimmer from "../Shimmer/VideoBackgroundShimmer";
+import VideoTitleShimmer from "../Shimmer/VideoTitleShimmer";
 
 const MainContainer = () => {
   const movies = useSelector((store) => store.movies?.nowPlayingMovies);
@@ -23,13 +25,24 @@ const MainContainer = () => {
 
   return (
       <div className="relative">
-        <VideoTitle
+        {!mainMovie ? (
+          <>
+          <div className="absolute bg-gradient-to-r from-black w-screen aspect-video">
+            <VideoTitleShimmer />
+          </div>
+          <VideoBackgroundShimmer />
+          </>
+        ) : (
+          <>
+          <VideoTitle
           original_title={original_title}
           overview={overview}
           id={id}
-        />
-        <VideoBackground movieId={id} />
-      </div>
+          />
+          <VideoBackground movieId={id} />
+          </>
+        )}
+          </div>
   );
 };
 
